d23668bcb7db8400b8fa6ed8737730f8b34b9179
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2019-06-12 21:47:46 +0200

    [ADD] Parse parameters according to custom options

--- src/parser.cpp
+++ src/parser.cpp
@@ -19,35 +19,56 @@ namespace Parser {
         for (const std::vector<std::string> &opt : custom_options) {
             Options::_keys.push_back(opt);
         }
+
+        Options::options.clear();
+        Options::set_options(argc, argv);
     }
 
-    Options::Options(const int argc, char *argv[]) {
-        for (int i = 0; i < Options::_keys.size(); i++) {
-            Options::options[Options::_keys[i][0]] = 0;
-        }
+    Options::Options(
+            std::vector<std::vector<std::string>> custom_options
+    ) {
+        Options::_keys.clear();
 
+        for (const std::vector<std::string> &opt : custom_options) {
+            Options::_keys.push_back(opt);
+        }
+    }
+
+    void Options::set_options(const int argc, char *argv[]) {
         std::string value;
         for (int i = 1; i < argc; i++) {
             value = static_cast<std::string>(argv[i]);
 
             std::string short_par;
             std::string long_par;
+            std::string short_sep = "-";
+            std::string long_sep = "--";
             for (int j = 0; j < Options::_keys.size(); j++) {
                 long_par = Options::_keys[j][0];
                 short_par = Options::_keys[j][1];
 
-                bool found_long = value == "--" + long_par;
-                bool found_short = (
-                    short_par != "" && value == "-" + short_par
+                bool found_long = (
+                    (long_par != "") && (value == long_sep + long_par)
                 );
-
-                if (found_long || found_short)
+                bool found_short = (
+                    (short_par != "") && (value == short_sep + short_par)
+                );
+                if (found_long && found_short) {
                     Options::options[Options::_keys[j][0]] = 1;
+                } else if (found_long && !found_short) {
+                    Options::options[Options::_keys[j][0]] = 1;
+                } else if (!found_long && found_short) {
+                    Options::options[Options::_keys[j][1]] = 1;
+                } else if (!found_long && !found_short) {
+                    // nothing
+                }
             }
         }
     }
 
     Options::Options(const int argc, char *argv[]) {
+        Options::options.clear();
+        Options::set_options(argc, argv);
     }
 
     std::vector<std::string> Options::keys(void) {
